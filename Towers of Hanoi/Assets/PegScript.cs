d09744c36fdb245ec28aa034f2c1270aca1cd3a8
Bethany Drake <bethanyd@thoughtworks.com>
2019-08-10 19:34:28 +1000

    when you click on a peg, the segment lifts up

--- Towers of Hanoi/Assets/PegScript.cs
+++ Towers of Hanoi/Assets/PegScript.cs
@@ -8,11 +8,16 @@ public class PegScript : MonoBehaviour
 
     void OnMouseDown(){
         Debug.Log("clicked!!");
+        GameObject topSegment = segments[segments.Count -1];
+        (topSegment.GetComponent<SegmentScript>() as SegmentScript).Lift();
+        Debug.Log(topSegment);
     }
     public List<GameObject> segments = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
+
+
 
     }
 
---
+++ Towers of Hanoi/Assets/SegmentScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegmentScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool rising;
+
+
+    public float speed;
+    public float maxHeight;
+
+    public void Lift() {
+
+        GetComponent<Rigidbody>().isKinematic = true;
+        rising = true;
+
+    }
+
+
+    void Start()
+    {
+        rising = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rising) {
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed *Time.deltaTime, transform.position.z);
+            if (transform.position.y >= maxHeight) {
+                transform.position = new Vector3(transform.position.x, maxHeight, transform.position.z);
+                rising = false;
+            }
+        }
+    }
+}
