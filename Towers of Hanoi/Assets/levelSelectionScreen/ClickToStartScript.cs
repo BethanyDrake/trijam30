ac15ce0d775f94854b3fdafe9f092bccac1ca269
Bethany Drake <bethanyd@thoughtworks.com>
2019-09-20 23:32:31 +1000

    level select screen

---
+++ Towers of Hanoi/Assets/levelSelectionScreen/ClickToStartScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToStartScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void BackToStartScreen()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    public void StartLevel1()
+    {
+        SceneManager.LoadScene("Wombat");
+    }
+    public void StartLevel2()
+    {
+        SceneManager.LoadScene("Echidna");
+    }
+    public void StartLevel3()
+    {
+        SceneManager.LoadScene("Koala");
+    }
+
+     public void StartLevel4()
+    {
+        SceneManager.LoadScene("Wallaby");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetMouseButtonDown(0)) {
+        //     SceneManager.LoadScene("MainScene");
+        // }
+    }
+}
---
+++ Towers of Hanoi/Assets/levelSelectionScreen/HighScoreController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class HighScoreController : MonoBehaviour
+{
+
+    public static int[] highscores = {0, 0, 0};
+
+    public Text[] texts = new Text[3];
+
+    public static void UpdateHighscore(int score, int level)
+    {
+        var highscore = highscores[level];
+        if (score > highscore)
+        {
+            highscores[level] = score;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++) {
+            var text = texts[i];
+            var score = highscores[i];
+            text.text = "Highscore: "+ score;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
