45024915069f6a17f1e74632c76699df2e6a8bd6
Bethany Drake <bethanyd@thoughtworks.com>
2019-09-20 19:43:54 +1000

    3 colour level

---
+++ Towers of Hanoi/Assets/WallabyWinChecker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallabyWinChecker : WinChecker
+{
+
+     public override bool CheckWinState(GameObject[] pegs) {
+        // Debug.Log("checking win state!");
+
+        int numCompleteTowers = 0;
+        foreach(GameObject peg in pegs) {
+            if ((peg.GetComponent<PegScript>() as PegScript).HasCompleteTower()) {
+                numCompleteTowers +=1;
+            }
+        }
+        Debug.Log(numCompleteTowers);
+         if (numCompleteTowers == 3) {
+            return true;
+        }
+        return false;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
